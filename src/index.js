287b28489d023f8a7920620a3334f89591238c9d
WenderMachado <wenderpereira396@gmail.com>
2024-02-13 19:46:29 -0300

    usei estruturas de decsões para resoler o desafio

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log("oi")
+
+
+
+let nomeDoHeroi= "Maximus"
+let BomLevel =10000
+let level= 1000
+let midLevel = 5000
+let hightLevel =10000
+
+
+if (BomLevel ===1000) {
+    console.log ("O heroi de nome " + nomeDoHeroi + "está num nivel baixo")
+} else if (BomLevel ===10000) {
+    console.log ("o heroi de nome " + nomeDoHeroi + " está no nivel imortal")
+}
+else{
+    console.log("o heroi ainda não está suficientemnte bom")
+}
