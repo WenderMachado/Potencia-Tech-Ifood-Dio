b47ecc50f88b46e7cf69f06fed4a4c03ee1fdcab
WenderMachado <wenderpereira396@gmail.com>
2024-02-17 18:44:54 -0300

    refatorado e refeito o desafio, revendo as aulas

---
+++ desafio2.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+ranking (35, 12)
+
+
+function ranking(win, lost){
+let result = win - lost
+console.log(result)
+if(){
+    result= >25
+}
+}
--- src/index.js
+++ src/index.js
@@ -1,18 +1,29 @@
 
 
 
-let nomeDoHeroi= "Maximus"
-let BomLevel =10000
-let level= 1000
-let midLevel = 5000
-let hightLevel =10000
 
+let nomeVilao = "Carlinhos";
+let xpHeroi = 9500;
+let lvl = ["Fraco", "Bronze", "Prata", "Ouro", "Platina", "Ascendente", "Imortal"];
 
-if (BomLevel ===1000) {
-    console.log ("O heroi de nome " + nomeDoHeroi + "está num nivel baixo")
-} else if (BomLevel ===10000) {
-    console.log ("o heroi de nome " + nomeDoHeroi + " está no nivel imortal")
-}
-else{
-    console.log("o heroi ainda não está suficientemnte bom")
+switch (true) {
+    case xpHeroi <= 1000:
+        console.log("O heroi de nome " + nomeVilao + " está no nível " + lvl[0]);
+        break;
+    case xpHeroi >= 1001 && xpHeroi <= 2000:
+        console.log("O heroi de nome " + nomeVilao + " está no nível " + lvl[1]);
+        break;
+    case xpHeroi >= 2001 && xpHeroi <= 5000:
+        console.log("O heroi de nome " + nomeVilao + " está no nível " + lvl[2]);
+        break;
+    case xpHeroi >= 5001 && xpHeroi <= 7000:
+        console.log("O heroi de nome " + nomeVilao + " está no nível " + lvl[3]);
+        break;
+    case xpHeroi >= 7001 && xpHeroi <= 9000:
+        console.log("O heroi de nome " + nomeVilao + " está no nível " + lvl[4]);
+        break;
+    case xpHeroi >= 9001 && xpHeroi <= 10000:
+        console.log("O heroi de nome " + nomeVilao + " está no nível " + lvl[5]);
+        break;
+    
 }
