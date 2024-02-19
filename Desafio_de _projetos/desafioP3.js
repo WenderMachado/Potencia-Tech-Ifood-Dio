f51ffc70cfa3bf3f44a1a2c484096c7b2297d713
WenderMachado <wenderpereira396@gmail.com>
2024-02-19 19:48:18 -0300

    finaizando projeto 3 e mandando descrição dos projetos

---
+++ Desafio_de _projetos/desafioP3.js
@@ -0,0 +1,98 @@
+
+// Instruções para entrega
+// # 3️ Escrevendo as classes de um Jogo
+
+// **O Que deve ser utilizado**
+
+// - Variáveis
+// - Operadores
+// - Laços de repetição
+// - Estruturas de decisões
+// - Funções
+// - Classes e Objetos
+
+// ## Objetivo:
+
+// Crie uma classe generica que represente um herói de uma aventura e que possua as seguintes propriedades:
+
+// - nome
+// - idade
+// - tipo (ex: guerreiro, mago, monge, ninja )
+
+// além disso, deve ter um método chamado atacar que deve atender os seguientes requisitos:
+
+// - exibir a mensagem: "o {tipo} atacou usando {ataque}")
+// - aonde o {tipo} deve ser concatenando o tipo que está na propriedade da classe
+// - e no {ataque} deve seguir uma descrição diferente conforme o tipo, seguindo a tabela abaixo:
+
+// se mago -> no ataque exibir (usou magia)
+// se guerreiro -> no ataque exibir (usou espada)
+// se monge -> no ataque exibir (usou artes marciais)
+// se ninja -> no ataque exibir (usou shuriken)
+
+// ## Saída
+
+// Ao final deve se exibir uma mensagem:
+
+// - "o {tipo} atacou usando {ataque}"
+//   ex: mago atacou usando magia
+//   guerreiro atacou usando espada
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Vilao {
+    constructor(nome, idade, tipo) {
+        this.nome =nome;
+        this.idade =idade;
+        this.tipo =tipo;
+    }
+
+    atacar() {
+        let ataque = "";
+        switch (this.tipo) {
+            case "mago":
+                ataque = "usou magia";
+                break;
+            case "guerreiro":
+                ataque = "usou espada";
+                break;
+            case "monge":
+                ataque = "usou artes marciais";
+                break;
+            case "ninja":
+                ataque = "usou shuriken";
+                break;
+            default:
+                ataque = "usou um ataque indefinido";
+        }
+
+        console.log(`O ${this.tipo} ${this.nome} atacou usando ${ataque}`);
+    }
+}
+
+let vilao = new Vilao("Ronaldinho", 30, "mago");
+vilao.atacar();
