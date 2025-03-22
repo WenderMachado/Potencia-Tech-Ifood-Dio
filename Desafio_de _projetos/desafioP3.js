eaabe9128cbe897065d26ac816eef0516d90ca6f
WenderMachado <143142413+WenderMachado@users.noreply.github.com>
2025-03-22 07:17:44 -0300

    Update 2025 desafioP3.js
    Ok

--- Desafio_de _projetos/desafioP3.js
+++ Desafio_de _projetos/desafioP3.js
@@ -41,33 +41,33 @@
 
 class Vilao {
     constructor(nome, idade, tipo) {
-        this.nome =nome;
-        this.idade =idade;
-        this.tipo =tipo;
+        this.nome =nome
+        this.idade =idade
+        this.tipo =tipo
     }
 
     atacar() {
-        let ataque = "";
+        let ataque = ""
         switch (this.tipo) {
             case "mago":
-                ataque = "usou magia";
+                ataque = "usou magia"
                 break;
             case "guerreiro":
-                ataque = "usou espada";
+                ataque = "usou espada"
                 break;
             case "monge":
-                ataque = "usou artes marciais";
+                ataque = "usou artes marciais"
                 break;
             case "ninja":
-                ataque = "usou shuriken";
+                ataque = "usou shuriken"
                 break;
             default:
-                ataque = "usou um ataque indefinido";
+                ataque = "usou um ataque indefinido"
         }
 
         console.log(`O ${this.tipo} ${this.nome} atacou usando ${ataque}`);
     }
 }
 
-let vilao = new Vilao("Ronaldinho", 30, "mago");
-vilao.atacar();
+let vilao = new Vilao("Ronaldinho", 30, "mago")
+vilao.atacar()
